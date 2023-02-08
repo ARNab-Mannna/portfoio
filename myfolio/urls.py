9455e84a902650b02f4f99fe7b7c7549e172bd59
Arnab <mannaarnab63@gmail.com>
2023-02-08 10:45:12 +0530

    gitignore

--- myfolio/urls.py
+++ myfolio/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('port_home.urls')),
 ]
+# git remote set-url origin https://ARNab-Mannna@github.com/ARNab-Mannna/portfoio.git
